a987bdf2e5d8523e2514e99d6d97456ee1b6405f
Panu Tanavatavivat <markpanya@gmail.com>
2023-02-10 13:27:43 +0700

    Push first React to git

---
+++ Simple React/index.js
@@ -0,0 +1,10 @@
+// React render header 1
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+
+// React render paragraph
+ReactDOM.render(<p>Hi, my name is Mark!</p>, document.getElementById("root2"))
+
+ReactDOM.render(
+    <ul><li>Thing 1</li><li>Thing 2</li></ul>,
+    document.getElementById("root3")
+)
